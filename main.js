686c7a19dc8ae3a0d7fed360d341d317e799806f
Anatoliy <anatoliy2221@gmail.com>
2024-09-14 22:16:40 +0300

    create markup

---
+++ main.js
@@ -0,0 +1,24 @@
+// import axios from "axios";
+
+// async function getYouTube() {
+//   const options = {
+//     method: "GET",
+//     url: "https://youtube-video-idea.p.rapidapi.com/youtube-idea",
+//     params: {
+//       topic: "digital marketing",
+//     },
+//     headers: {
+//       "X-RapidAPI-Key": "60d55d37bdmsh44e099a8b34f95dp14577fjsna07f8fa5ca16",
+//       "X-RapidAPI-Host": "youtube-video-idea.p.rapidapi.com",
+//     },
+//   };
+
+//   try {
+//     const response = await axios.request(options);
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+//console.log(getYouTube());
